944a1aba00ad7a055b0e530b6125374152f2fafd
lincait <3250360142@qq.com>
2026-05-28 02:55:58 -0400

    更改编译目录，忽略build文件夹

--- 01_C_basics/array.c
+++ 01_C_basics/array.c
@@ -87,10 +87,70 @@ static void Prime_num(void)
     
 }
 
+/*求二维数组每行和、每列和、总和（不用额外变量存储）*/
+static void Sum(void)
+{
+    //数据为4行3列，则创建5行4列数组，每行每列的最后一个存放对应和
+    int a[5][4] = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            a[4][3] += a[i][j]; //最后一个格子存放总和
+            a[4][j] += a[i][j]; //最后一行依次分别加上当前行所在列（一次外层循环，每个列和分别加一次）
+            a[i][3] += a[i][j]; //最后一列依次加上当前行全部列（一次外层循环算完一个行和）
+        }
+        
+    }
+    
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
+
+/*矩阵乘积*/
+static void matri_mul(void)
+{
+    int a[3][4] = {{4,5,6,7},{3,1,1,1},{1,2,3,5}};
+    int b[4][3] = {{0,0,0},{1,1,1},{2,2,2},{3,3,3}};
+    int mul[3][3];  //a*b
+
+    for(int i = 0; i < 3; i ++)         //每轮循环计算完mul的一行
+    {
+        for(int j = 0; j < 3; j ++)     //每轮循环计算完两个向量内积（一行乘一列）
+        {
+            for (int k = 0; k < 4; k++) //每轮循环计算两个元素乘积
+            {
+                mul[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    //显示输出结果
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d\t",mul[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
 int main()
 {
     // Buble_sort();
     // Select_sort();
-    Prime_num();
+    // Prime_num();
+    // Sum();
+    matri_mul();
     return 0;
 }
